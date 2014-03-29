6f835628ff604cc455839edae57c234a490ed5b4
John Lawrence <johnonrails@gmail.com>
2014-03-28 17:24:52 -0700

    Add selected class to li

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,14 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.where(week: params[:week])
     @message_counts = Message.order(:week).select('week, count(*) qty').group(:week)
+
+    if params.has_key?(:week)
+      @week = params.delete('week').to_i - 1
+      if @week < 0 || @week > WEEK_TOPICS.count - 1
+        redirect_to params
+      else
+        @messages = Message.where(week: @week)
+      end
+    end
   end
 end
