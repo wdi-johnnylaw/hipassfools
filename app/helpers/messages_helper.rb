1f0fb16710a0bfdad18a0e4f41417c2cd15dbf91
John Lawrence <johnonrails@gmail.com>
2014-04-01 17:01:46 -0700

    Redact @names from messages for public view

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,6 +2,7 @@ module MessagesHelper
   def linkify(text)
     text = h(text).gsub('&quot;', '"')
     text.gsub!(/(https?:\/\/[^\s"]+)/, '<a href="\1" target="_blank">\1</a>')
+    text.gsub!(/@(\w{2})\w+/, '@\1------') unless user_signed_in?
     text.sub(/^\/code\s(.*)$/, '<pre>\1</pre>').
          gsub(/\\n/, "\n").html_safe
   end
